065204813747506526b1841aea4687d2b2a2a427
Neon <neon1226@yandex.ru>
2019-09-25 08:14:16 +0400

    Setting up db tables

---
+++ entityframework/Entities.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+namespace entityframework
+{
+    class Entities : DbContext
+    {
+        public Entities():base("DbConnection") { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Base> Bases { get; set; }
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Squad> Squads { get; set; }
+
+        /*protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<Entities>(null);
+            base.OnModelCreating(modelBuilder);
+        }*/
+    }
+}
--- entityframework/Program.cs
+++ entityframework/Program.cs
@@ -1,32 +1,53 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace entityframework
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            using (UserContext db = new UserContext())
-            {
-                // создаем два объекта User
-                //User user1 = new User { Name = "Tom", Age = 33 };
-                //User user2 = new User { Name = "Sam", Age = 26 };
+            Console.WriteLine("Введите команду:\n1 - вывод содержимого таблиц\n2 - заполнить таблицы");
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.Clear();
 
-                // добавляем их в бд
-                //db.Users.Add(user1);
-                //db.Users.Add(user2);
-                //db.SaveChanges();
-                //Console.WriteLine("Объекты успешно сохранены");
-
-                // получаем объекты из бд и выводим на консоль
-                var users = db.Users;
-                Console.WriteLine("Список объектов:");
-                foreach (User u in users)
+            using (Entities db = new Entities())
+                if (key.KeyChar == '2')
                 {
-                    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Name, u.Age);
+                    User user1 = new User { ID = 0, Name = "Admin", Password = "123456", Role = 1, Wins = 0, Loses = 0 }; 
+                    User user2 = new User { ID = 1, Name = "User", Password = "123", Role = 0, Wins = 0, Loses = 0 };
+                    db.Users.Add(user1);
+                    db.Users.Add(user2);
+                    Base base1 = new Base { Name = "AdminBase", Owner = 0, Credits = 100, Energy = 100, CoordX = 1, CoordY = 1, Level = 0 };
+                    Base base2 = new Base { Name = "UserBase", Owner = 1, Credits = 100, Energy = 100, CoordX = 4, CoordY = 2, Level = 0 };
+                    db.Bases.Add(base1);
+                    db.Bases.Add(base2);
+                    db.SaveChanges();
+                    Console.WriteLine("Объекты успешно сохранены");
                 }
-            }
+                else
+                {
+                    var users = db.Users;
+                    Console.WriteLine("Список объектов:");
+                    foreach (User u in users)
+                    {
+                        Console.WriteLine("{0}.{1} - {2}", u.ID, u.Name, u.Password);
+                    }
+                    var bases = db.Bases;
+                    Console.WriteLine("Список объектов:");
+                    foreach (Base u in bases)
+                    {
+                        Console.WriteLine("{0}.{1} - {2}. Coords: {3}x{4}", u.ID, u.Name, u.Owner, u.CoordX, u.CoordY);
+                    }
+                }
             Console.Read();
         }
     }
 }
+
+/*foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(user2))
+{
+    string name = descriptor.Name;
+    object value = descriptor.GetValue(user2);
+    Console.WriteLine("{0}={1}", name, value);
+}*/
